e918362ae2520a089f829d97ab6d0e7872aa3b9f
levchigintsev <lev.chigintsev@gmail.com>
2026-06-07 18:57:07 +0300

    root card bookmark animation

--- lib/view/widgets/root_card.dart
+++ lib/view/widgets/root_card.dart
@@ -1,16 +1,16 @@
 import 'package:almi3/core/app_colors.dart';
 import 'package:almi3/view/widgets/root_bookmark.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class RootCard extends StatelessWidget {
+class RootCard extends StatefulWidget {
   final String hebrewText;
   final int adjCount;
   final int verbCount;
   final int nounCount;
   final bool isBookmarked;
   final VoidCallback? onTap;
-  final VoidCallback? onBookmarkTap;
 
   const RootCard({
     super.key,
@@ -20,72 +20,116 @@ class RootCard extends StatelessWidget {
     this.nounCount = 0,
     this.isBookmarked = false,
     this.onTap,
-    this.onBookmarkTap,
   });
+
+  @override
+  State<RootCard> createState() => _RootCardState();
+}
+
+class _RootCardState extends State<RootCard> with SingleTickerProviderStateMixin {
+  late bool _isBookmarked;
+  late final AnimationController _pulseController;
+  late final Animation<double> _scale;
+
+  static const _bookmarkHeight = 37.0;
+
+  @override
+  void initState() {
+    super.initState();
+    _isBookmarked = widget.isBookmarked;
+    _pulseController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+    );
+    _scale = TweenSequence([
+      TweenSequenceItem(tween: Tween(begin: 1.0, end: 1.06), weight: 1),
+      TweenSequenceItem(tween: Tween(begin: 1.06, end: 1.0), weight: 1),
+    ]).animate(CurvedAnimation(parent: _pulseController, curve: Curves.easeInOut));
+  }
+
+  @override
+  void dispose() {
+    _pulseController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        height: 100,
-        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 5),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(12),
-          border: Border.all(color: AppColors.cardBorder),
-          gradient: const LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [AppColors.rootCard, AppColors.rootCardGradient],
+      onTap: widget.onTap,
+      onLongPress: () {
+        HapticFeedback.mediumImpact();
+        _pulseController.forward(from: 0);
+        setState(() => _isBookmarked = !_isBookmarked);
+      },
+      child: ScaleTransition(
+        scale: _scale,
+        child: Container(
+          height: 100,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 5),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            border: Border.all(color: AppColors.cardBorder),
+            gradient: const LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [AppColors.rootCard, AppColors.rootCardGradient],
+            ),
+            boxShadow: const [
+              BoxShadow(color: Color(0x35000000), blurRadius: 8, spreadRadius: 1, offset: Offset(0, 3)),
+            ],
           ),
-          boxShadow: const [
-            BoxShadow(color: Color(0x35000000), blurRadius: 8, spreadRadius: 1, offset: Offset(0, 3)),
-          ],
-        ),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(12),
-          child: Stack(
-            children: [
-              Positioned.fill(
-                bottom: 5,
-                child: Center(
-                  child: Text(
-                    hebrewText,
-                    textAlign: TextAlign.center,
-                    textDirection: TextDirection.rtl,
-                    style: GoogleFonts.notoSansHebrew(
-                      fontSize: 66,
-                      fontWeight: FontWeight.w800,
-                      color: AppColors.rootCardText,
-                      height: 1.0,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(12),
+            child: Stack(
+              children: [
+                Positioned.fill(
+                  bottom: 5,
+                  child: Center(
+                    child: Text(
+                      widget.hebrewText,
+                      textAlign: TextAlign.center,
+                      textDirection: TextDirection.rtl,
+                      style: GoogleFonts.notoSansHebrew(
+                        fontSize: 66,
+                        fontWeight: FontWeight.w800,
+                        color: AppColors.rootCardText,
+                        height: 1.0,
+                      ),
                     ),
                   ),
                 ),
-              ),
-              Positioned(
-                left: 0,
-                right: 0,
-                bottom: 0,
-                child: _ProgressBar(adjCount: adjCount, verbCount: verbCount, nounCount: nounCount),
-              ),
-              Positioned(
-                left: 16,
-                top: 0,
-                bottom: 5,
-                child: Center(
-                  child: _BadgeGroup(adjCount: adjCount, verbCount: verbCount, nounCount: nounCount),
-                ),
-              ),
-              if (isBookmarked)
                 Positioned(
-                  top: 0,
-                  right: 25,
-                  child: GestureDetector(
-                    onTap: onBookmarkTap,
-                    child: const BookmarkRibbon(),
+                  left: 0,
+                  right: 0,
+                  bottom: 0,
+                  child: _ProgressBar(
+                    adjCount: widget.adjCount,
+                    verbCount: widget.verbCount,
+                    nounCount: widget.nounCount,
                   ),
                 ),
-            ],
+                Positioned(
+                  left: 16,
+                  top: 0,
+                  bottom: 5,
+                  child: Center(
+                    child: _BadgeGroup(
+                      adjCount: widget.adjCount,
+                      verbCount: widget.verbCount,
+                      nounCount: widget.nounCount,
+                    ),
+                  ),
+                ),
+                AnimatedPositioned(
+                  duration: const Duration(milliseconds: 200),
+                  curve: Curves.easeOut,
+                  top: _isBookmarked ? 0 : -_bookmarkHeight,
+                  right: 25,
+                  child: const BookmarkRibbon(),
+                ),
+              ],
+            ),
           ),
         ),
       ),
